84af8e3ae3aa1eb7cb91bfbe7b30f33a19e64f23
SanaAkhanova <76734482+SanaAkhanova@users.noreply.github.com>
2021-02-12 20:14:25 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+import telebot
+from telebot import types
+import requests, bs4
+import flask
+
+appname = 'sana-dont-delet-move-bot'
+
+
+link = 'https://filmix.co/'
+
+token = '1519585389:AAFplChhTPvZtxXqJbmxNtwhqYiUO5fJxQY'
+server = flask.Flask(__name__)
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    k = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    k.add(*[types.KeyboardButton(name) for name in ['Комедии',
+                                                    'Боевики',
+                                                    'Ужасы',
+                                                    'Аниме',
+                                                    'Документальные',
+                                                    'Детектив',
+                                                    'Драмы']])
+    m = bot.send_message(message.chat.id, 'Фильм какого жанра вы хотели бы посмотреть?', reply_markup=k)
+    bot.register_next_step_handler(m, movie)
+    
+    
+@bot.message_handler(content_types=['text'])
+def movie(message):
+    global link
+    if message.text == 'Комедии':
+        link = link + 'komedia/'
+    elif message.text == 'Боевики':
+        link = link + 'boevik/'
+    elif message.text == 'Ужасы':
+        link = link + 'uzhasu/'
+    elif message.text == 'Аниме':
+        link = link + 'animes/'
+    elif message.text == 'Документальные':
+        link = link + 'dokumentalenyj/'
+    elif message.text == 'Детектив':
+        link = link + 'detektivy/'
+    elif message.text == 'Драмы':
+        link = link + 'drama/'
+    parsing(message)
+
+def parsing(message):
+    global link
+    data = requests.get(link)
+    soup = bs4.BeautifulSoup(data.text, 'html.parser')
+    links = soup.findAll('a') #ищем все теги '<a>'
+    for l in links:
+        href = l.get('href')
+        if href.endswith('.html'):
+            bot.send_message(message.chat.id, href)
+
+
+@server.route('/' + token, methods=['POST'])
+def get_message():
+     bot.process_new_updates([types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
+     return "!", 200
+
+@server.route('/', methods=["GET"])
+def index():
+     bot.remove_webhook()
+     bot.set_webhook(url=f"https://{appname}.herokuapp.com/{token}")
+     return "Hello from Heroku!", 200
+     
+
+if __name__ == "__main__":
+     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+
+#bot.polling(none_stop=True)
